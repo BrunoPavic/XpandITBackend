c0dcd644b18616e175b18864e56e5be74ecfd5da
Bruno Pavić <bruno.pavic@shape404.agency>
2022-10-13 18:07:54 +0200

    Added swagger documentation
    swagger documentation

--- src/main/java/com/xpandit/challenge/XpandItBackendApplication.java
+++ src/main/java/com/xpandit/challenge/XpandItBackendApplication.java
@@ -3,7 +3,11 @@ package com.xpandit.challenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Xpand challenge - MoviesAPI", version = "1.0.0", description = "API documentation for Xpand challenge"))
 public class XpandItBackendApplication {
 	
 	public static void main(String[] args) {
